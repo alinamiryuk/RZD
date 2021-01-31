c7b2eec3fbc0c9f3993f7e257f3195ef9e5206e1
alinamiryuk <alinamiryuk@mail.ru>
2021-01-31 23:26:32 +0300

    breadcrumbs menu

--- frontend/src/components/Breadcrumbs/Breadcrumbs.js
+++ frontend/src/components/Breadcrumbs/Breadcrumbs.js
@@ -1,5 +1,32 @@
 import React from 'react'
+import { Step, Icon, Grid, Menu, Dropdown } from 'semantic-ui-react'
+import './Breadcrumbs.css'
 
 export const Breadcrumbs = () => {
-  return <div>Breadcrumbs</div>
+  return (
+    <Grid className="breadcrumbs">
+      <Menu attached="top">
+        <Dropdown item icon="bars" simple>
+          <Dropdown.Menu>
+            <Dropdown.Item>Популярные направления</Dropdown.Item>
+            <Dropdown.Item>Поезда</Dropdown.Item>
+            <Dropdown.Item>Новости и скидки</Dropdown.Item>
+            <Dropdown.Item>Вернуть билет</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <b>Меню</b>
+      </Menu>
+      <Step.Group>
+        <Step>
+          <Step.Title>Поиск билета</Step.Title>
+        </Step>
+        <Step>
+          <Step.Title>Выбор поезда и билета</Step.Title>
+        </Step>
+        <Step active>
+          <Step.Title>Данные пассажиров и оплата</Step.Title>
+        </Step>
+      </Step.Group>
+    </Grid>
+  )
 }
